adf8dcd698fc3506b0c30411f96269b1d8a6ddcc
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-11-21 22:27:46 +0500

    - 'Request invitation' button is inactive when input box is empty & the content in the input box is not a valid email address. - Show a response message after clicking on 'Request invitation' button. - Clear the input box after invitation has been sent.

---
+++ app/controllers/index.js
@@ -0,0 +1,21 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { match, not } from '@ember/object/computed';
+
+export default class IndexController extends Controller {
+  @tracked emailAddress = '';
+  @tracked responseMessage = '';
+
+  @match('emailAddress', /^.+@.+\..+$/) isValid;
+  @not('isValid') isDisabled;
+
+  @action
+  saveInvitation() {
+    alert(
+      `Saving of the following email address is in progress: ${this.emailAddress}`
+    );
+    this.responseMessage = `Thank you! We've just saved your email address: ${this.emailAddress}`;
+    this.emailAddress = '';
+  }
+}
