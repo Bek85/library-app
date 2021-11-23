492ebe25bdb56178cf721441d3fe962ad53c2727
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-11-23 19:33:29 +0500

    generate libraries/edit with dynamic segment

--- app/router.js
+++ app/router.js
@@ -16,5 +16,6 @@ Router.map(function () {
   });
   this.route('libraries', function () {
     this.route('new');
+    this.route('edit', { path: '/:library_id/edit' });
   });
 });
---
+++ app/routes/libraries/edit.js
@@ -0,0 +1,4 @@
+import Route from '@ember/routing/route';
+
+export default class LibrariesEditRoute extends Route {
+}
