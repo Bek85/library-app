7fe7e9fcabf9c3905f3c8efb150515efa5e3d19f
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-11-24 23:42:37 +0500

    delete a single invitation in admin/invitations page

--- app/controllers/admin/invitations.js
+++ app/controllers/admin/invitations.js
@@ -1,4 +1,10 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
 export default class AdminInvitationsController extends Controller {
+  @action
+  deleteInvitation(invitation) {
+    const confirmed = confirm('Are you sure?');
+    if (confirmed) invitation.destroyRecord();
+  }
 }
