350eb93aabe952c1d22eaf3f14e615b25f17d238
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-11-25 16:24:46 +0500

    refactor libraries/index template to library-item component

--- app/models/library.js
+++ app/models/library.js
@@ -1,7 +1,10 @@
 import Model, { attr } from '@ember-data/model';
+import { notEmpty } from '@ember/object/computed';
 
 export default class LibraryModel extends Model {
   @attr('string') name;
   @attr('string') address;
   @attr('string') phone;
+
+  @notEmpty('name') isValid;
 }
