155f199e7c6c98f9a8ccc3edcb4ec3cc31aaf250
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-11-25 23:15:23 +0500

    use rsvp in admin/seeder route to retrieve multiple models in the same route

--- app/routes/admin/seeder.js
+++ app/routes/admin/seeder.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
+import { hash } from 'rsvp';
 
 export default class AdminSeederRoute extends Route {
+  model() {
+    return hash({
+      libraries: this.store.findAll('library'),
+      books: this.store.findAll('book'),
+      authors: this.store.findAll('author'),
+    });
+  }
 }
