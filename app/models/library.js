9b1781346d88ee01c3bcd939837d3d735aaf614d
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-11-25 18:33:41 +0500

    generate admin/seeder route

--- app/models/library.js
+++ app/models/library.js
@@ -1,10 +1,11 @@
-import Model, { attr } from '@ember-data/model';
+import Model, { attr, hasMany } from '@ember-data/model';
 import { notEmpty } from '@ember/object/computed';
 
 export default class LibraryModel extends Model {
   @attr('string') name;
   @attr('string') address;
   @attr('string') phone;
+  @hasMany('book') books;
 
   @notEmpty('name') isValid;
 }
