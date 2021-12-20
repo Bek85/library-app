2fb5588d06d0e0e5e881b24c9bf7f2a7a2af9896
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-12-21 01:13:25 +0500

    create author-select and library-select components with markup and component logic

---
+++ app/controllers/books.js
@@ -0,0 +1,64 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class BooksController extends Controller {
+  @tracked authors = this.model.authors;
+  @tracked books = this.model.books;
+  @tracked libraries = this.model.libraries;
+
+  @action
+  editBook(book) {
+    book.isEditing = true;
+  }
+
+  @action
+  cancelBookEdit(book) {
+    book.isEditing = false;
+    book.rollbackAttributes();
+  }
+
+  @action
+  saveBook(book) {
+    if (book.isValid) {
+      book.isEditing = false;
+      book.save();
+    }
+  }
+
+  @action
+  editAuthor(book) {
+    book.isAuthorEditing = true;
+  }
+
+  @action
+  cancelAuthorEdit(book) {
+    book.isAuthorEditing = false;
+    book.rollbackAttributes();
+  }
+
+  @action
+  saveAuthor(author, book) {
+    book.author = author;
+    book.save();
+    book.isAuthorEditing = false;
+  }
+
+  @action
+  editLibrary(book) {
+    book.isLibraryEditing = true;
+  }
+
+  @action
+  cancelLibraryEdit(book) {
+    book.isLibraryEditing = false;
+    book.rollbackAttributes();
+  }
+
+  @action
+  saveLibrary(library, book) {
+    book.library = library;
+    book.save();
+    book.isLibraryEditing = false;
+  }
+}
--- app/routes/books.js
+++ app/routes/books.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
+import { hash } from 'rsvp';
 
 export default class BooksRoute extends Route {
+  model() {
+    return hash({
+      books: this.store.findAll('book'),
+      authors: this.store.findAll('author'),
+      libraries: this.store.findAll('library'),
+    });
+  }
 }
